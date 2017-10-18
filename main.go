b92af4292d35db0aa8e853e147ec60784611601d
Salvatore D'Agostino <salvatoredagostino@invisionapp.com>
2017-10-17 21:34:41 -0400

    implement process funciton

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+
+	"github.com/ttacon/emoji"
 
 	"github.com/gorilla/mux"
 )
@@ -39,6 +43,57 @@ func main() {
 
 // Process is the main RPC endpoint for this service that will manipulate a passed in string
 func Process(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello there and welcome to your service!")
-	// TODO: Implement me
+	// Parse string to wrap words with ":"
+	decoder := json.NewDecoder(r.Body)
+	var potato Potato
+	err := decoder.Decode(&potato)
+
+	if err != nil {
+		errorString := fmt.Errorf("Error parsing JSON string: %s", err)
+		w.Write([]byte(errorString.Error()))
+	}
+
+	emoji, err := buildEmojiString(potato)
+	if err != nil {
+		errorString := fmt.Errorf("Could not build Emoji string: %s", err)
+		w.Write([]byte(errorString.Error()))
+	}
+
+	var node string
+
+	node, err = os.Hostname()
+	if err != nil {
+		node = "iToto-Default-Host"
+	}
+
+	// Build Entry
+	entry := Entry{
+		Node: node,
+		Text: emoji,
+		Desc: "Convert words to Emojis",
+	}
+	potato.History = append(potato.History, entry)
+	json, _ := json.Marshal(potato)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
+}
+
+func buildEmojiString(potato Potato) (string, error) {
+	wordsArray := strings.Split(potato.Text, " ")
+
+	for i, word := range wordsArray {
+		// lower case
+		word = strings.ToLower(word)
+		// wrap with ':'
+		emojifyCol := ":" + word + ":"
+		// Check if we have an emoji for this word
+		emojify := emoji.Emoji(emojifyCol)
+		if strings.Compare(emojify, emojifyCol) != 0 {
+			// Overwrite word with emoji version of word
+			wordsArray[i] = emojify
+		}
+	}
+
+	return strings.Join(wordsArray, "  "), nil
 }
